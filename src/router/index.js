c34aa67b6d0512e423860d2b300f71761f8cd2bd
jiuli0212 <1602642164@qq.com>
2019-12-08 16:13:29 +0800

    注册提交1

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Register from '@/views/register'
 
 Vue.use(Router)
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      name: 'Register',
+      component: Register
+    },
+		{
+			path:'/Register',
+			name:"Register",
+			component:Register
+		}
   ]
 })
