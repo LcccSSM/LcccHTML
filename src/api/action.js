bc7c3fa2471e8cd2ff821d7a67faaa33ed17258d
likang <2454171623@qq.com>
2019-12-10 20:18:28 +0800

    注册功能，登录页面 LK

--- src/api/action.js
+++ src/api/action.js
@@ -3,13 +3,12 @@
  * 模块名_实体名_操作
  */
 export default {
-  'SERVER': 'http://localhost:8080/ssh2', //服务器
-  
-  
-  
-  'SYSTEM_GETMARKET': '/lccc/salChanceAction_get', //获取所有销售信息
+  'SERVER': 'http://localhost:8080/LcccSSM', //服务器
   'SYSTEM_GETSTORAGE': '/lccc/lcccStorageAction_getStorage',//产品库存
-
+	
+	'LCCCSSM_ADDUSER': '/addUser',//用户注册
+	'LCCCSSM_YZM': '/YZM',//手机短信
+	
 
    'getFullPath': k => { //获得请求的完整地址，用于mockjs测试时使用
     return this.SERVER + this[k];
--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,45 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import Register from '@/views/register'
+import App from '@/App'
+import Login from '@/views/Login'
+import Register from '@/views/Register'
+import a from '@/views/a'
+import Phone from '@/views/Phone'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Register',
-      component: Register
-    },
+	routes: [{
+			path: '/',
+			name: 'Login',
+			component: Login,
+// 			children:[{
+// 					path:'/Phone',
+// 					name:'Phone',
+// 					component:Phone
+// 			},]
+		},
+		{
+			path: '/Login',
+			name: "Login",
+			component: Login,
+		},
 		{
 			path:'/Register',
-			name:"Register",
+			name:'Register',
 			component:Register
-		}
-  ]
+		},
+		{
+			path:'/a',
+			name:'a',
+			component:a
+		},
+		{
+			path:'/Phone',
+			name:'Phone',
+			component:Phone
+		},
+		
+	]
 })
