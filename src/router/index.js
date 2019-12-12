73aa8244d14b0a609cf0b3b423de0f5a40134cbc
csq0616 <3279293229@qq.com>
2019-12-12 17:56:47 +0800

    字典及审核放款

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import DictIndex from '@/views/dict/DictIndex'
+import LoantIndex from '@/views/loan/LoantIndex'
+
 
 Vue.use(Router)
 
@@ -10,6 +13,16 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/dict/DictIndex',
+      name: 'DictIndex',
+      component: DictIndex
+    },
+    {
+      path: '/loan/LoantIndex',
+      name: 'LoantIndex',
+      component: LoantIndex
     }
   ]
 })
